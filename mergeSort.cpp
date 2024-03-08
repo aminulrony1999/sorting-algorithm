adc819ca9cebc8b9782638f811d61bb7cb1333c2
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-08 14:57:21 +0530

    updated

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+void merge()
+{
+
+}
+void mergeSort()
+{
+
+}
 int main()
 {
-    
+    int n; 
+    cin>>n;
+    vector<int> arr(n,0);
+    for(int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    return 0;
 }
